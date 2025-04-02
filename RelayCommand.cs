29211c9117308d5b43be6294cdb7360d0716c0e4
하진희 <jinhee@ati2000.co.kr>
2025-04-02 16:59:23 +0900

    RelayCommand 추가

---
+++ RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DefectViewProgram
+{
+    public class RelayCommand<T> : ICommand
+    {
+        Action<T> _execute;
+        Predicate<T> _canExecute;
+
+        public RelayCommand(Action<T> execute) : this(execute, null) { }
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (execute == null) throw new ArgumentNullException(nameof(execute));
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute.Invoke((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
